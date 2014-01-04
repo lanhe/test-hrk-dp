61eabee7cd0f5a86e5e1b25f3895840085af86a5
lanhe <lanhe@yahoo.co.jp>
2014-01-04 23:08:27 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestHrkDp::Application.config.secret_token = '7799793e9a94f70797a5d2df245308a4375dffee353b1067196d16db5fe1833fd232a95e8e9c643f3a3a33ad8ede6e935cde2b46bfd35e36fda38426d535076b'
